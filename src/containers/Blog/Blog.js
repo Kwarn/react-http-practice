bef9e4134329ec20882e61e20fa30455fd0263ff
Kwarn <karlwarner.dev@gmail.com>
2020-06-26 16:04:54 +0100

    Working on displaying selected post in FullPost

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,6 +8,7 @@ import './Blog.css'
 class Blog extends Component {
   state = {
     posts: [],
+    selectedPostId: null
   }
 
   componentDidMount() {
@@ -16,19 +17,30 @@ class Blog extends Component {
       const updatedPosts = post.map(post => {
         return { ...post, author: 'Karl' }
       })
-      this.setState({ posts: updatedPosts})
+      this.setState({ posts: updatedPosts })
     })
+  }
+
+  postSelectedHandler = id => {
+    this.setState({selectedPostId: id})
   }
 
   render() {
     const posts = this.state.posts.map(post => {
-      return <Post key={post.id} title={post.title} author={post.author} />
+      return (
+        <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)}
+        />
+      )
     })
     return (
       <div>
         <section className="Posts">{posts}</section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.selectedPostId} />
         </section>
         <section>
           <NewPost />
